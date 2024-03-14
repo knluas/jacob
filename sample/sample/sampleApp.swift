bb902cb362853c28ec0c8693fba761a607bd4114
jacob <knluas@naver.com>
2024-03-14 17:57:43 +0900

    first commit

---
+++ sample/sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by sunjongJang on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
